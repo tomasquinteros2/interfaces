e40f90240fdb7ae466ebdcd5f5a0f658c7570a4a
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2023-10-07 19:31:45 -0300

    todavia no anda el carrito

--- tp2/js/carrito.js
+++ tp2/js/carrito.js
@@ -6,7 +6,18 @@ let agregados = 0;
 
 agregables.forEach(agregable => {
     agregable.addEventListener('click',() => {
+        toggleShow();
         agregados++ ;
         carrito.innerHTML=agregados;
     });
 });
+
+function toggleShow(){
+    if(agregados>0){
+        carrito.classList.add("show");
+    }else if(agregados<=0){
+        carrito.classList.remove("show");
+    }
+    
+
+}
