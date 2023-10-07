55bb3e7a227dae377c7381665e12c6e3d694a565
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2023-10-07 18:58:44 -0300

    m

---
+++ tp2/js/carrito.js
@@ -0,0 +1,11 @@
+'use strict'
+let agregables = document.querySelectorAll(".btn-agregar");
+let carrito= document.querySelector("#carrito");
+let agregados = 0;
+
+agregables.forEach(agregable => {
+    agregable.addEventListener('click',agregaCarrito())
+});
+
+
+
