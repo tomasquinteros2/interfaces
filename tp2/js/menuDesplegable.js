dbf77637016826ff97a148277c35019528e3feb1
ticope <82914165+ticope@users.noreply.github.com>
2023-10-06 22:55:30 -0300

    carrito y register

--- tp2/js/menuDesplegable.js
+++ tp2/js/menuDesplegable.js
@@ -1,7 +1,9 @@
 'use strict'
 
 document.querySelector("#menuHome").addEventListener("click", toggleMenuH);
-document.querySelector("#userMenu").addEventListener("click", toggleMenuU)
+document.querySelector("#userMenu").addEventListener("click", toggleMenuU);
+document.querySelector("#cierraMenuH").addEventListener("click", toggleMenuH);
+document.querySelector("#cierraMenuU").addEventListener("click", toggleMenuU);
 
 function toggleMenuH(){
     document.querySelector("#menuH").classList.toggle("show");
