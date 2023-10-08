983ae95824cece4cad560ee5985a1d40179894b8
ticope <82914165+ticope@users.noreply.github.com>
2023-10-08 20:33:20 -0300

    animacionBtn-carrito

--- tp2/js/carrito.js
+++ tp2/js/carrito.js
@@ -2,11 +2,32 @@
 let agregables = document.querySelectorAll(".btn-agregar");
 let carrito= document.querySelector("#carrito");
 let agregados = 0;
+const estadosBotones=Array.from({ length: agregables.length }, () => false);
 
+agregables.forEach((agregable, index) => {
 
-agregables.forEach(agregable => {
+    estadosBotones[index]= false;
+
     agregable.addEventListener('click',() => {
-        agregados++ ;
-        carrito.innerHTML=agregados;
+
+        if(estadosBotones[index]){
+            agregados--;
+        }else{
+            agregados++;
+        }
+
+        toggleShow();
+        carrito.innerHTML= agregados;
+        estadosBotones[index] = !estadosBotones[index]; 
     });
 });
+
+function toggleShow(){
+    if(agregados>0){
+        carrito.classList.add("show");
+    }else if(agregados<=0){
+        carrito.classList.remove("show");
+    }
+    
+
+};
