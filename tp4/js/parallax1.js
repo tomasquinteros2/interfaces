b1ea45582d39d505a9774fce76bfabae36b577ba
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2023-11-17 16:17:10 -0300

    se llama gwen no ween

---
+++ tp4/js/parallax1.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const grupo1 = document.getElementById("section-one");
+const edificioCentro = document.getElementById("img-edificioCentro");
+const edificioIzq = document.getElementById("img-edificioIzq");
+const edificioDerecha = document.getElementById("ing-edificioDerecha");
+const milesM= document.getElementById("img-miles");
+const spiderMan= document.getElementById("img-spider-man");
+const gwenStacy= document.getElementById("img-gwen");
+
