800599ab6e803573cbc9e18831632fbcabc40827
ticope <82914165+ticope@users.noreply.github.com>
2023-11-20 14:56:38 -0300

    animacionHover

--- tp4/js/grupo8.js
+++ tp4/js/grupo8.js
@@ -1,4 +1,5 @@
 'use strict'
+let section = document.querySelector("#fondogp8");
 
 let gwen = document.getElementById('gwen');
 let miles = document.getElementById('miles-morales');
@@ -6,60 +7,74 @@ let spiderMan = document.getElementById('spider-man');
 
 let containerSpiders= document.getElementById("containerGp8");
 let layer = document.getElementById("layerGp8");
-let fondo= document.getElementById("fondogp8");
 
+let grupoInicial = document.querySelector("#grupoInicial")
+let grupoMorales = document.querySelector("#grupoMorales");
+let grupoGwen = document.querySelector("#grupoGwen")
+let grupoParker = document.querySelector("#grupoParker")
+
+let fondo = document.querySelector("#fondogp8");
+let lineaArriba = document.querySelector("#line1");
+let lineaAbajo = document.querySelector("#line2");
 gwen.addEventListener('mouseover', function(){
-    container.style.borderTop= "10px solid #C92B94";
-    container.style.borderBottom= "10px solid #C92B94";
-    layer.style.backgroundColor= "rgba(255, 0, 167, 0.2)";
-    fondo.style.opacity="0";
-    gwen.style.scale="1.5";
-    spiderMan.style.scale="0.5";
-    miles.style.scale="0.5";
+    gwen.style.display = "block";
+    grupoGwen.style.display = "block";
+    miles.style.display = "none";
+    spiderMan.style.display = "none";
+    gwen.style.scale = 1.5;
+    gwen.style.top = 200+"px";
+    fondo.src = "img/fondos//grupo4/Rectangle 10.png"
+    lineaArriba.src = "img/fondos//grupo4/linea10Up.png"
+    lineaAbajo.src = "img/fondos//grupo4/linea10Down.png"
 });
 gwen.addEventListener('mouseout', function(){
-    container.style.borderTop= "none";
-    container.style.borderBottom= "none";
-    layer.style.backgroundColor= "transparent";
-    fondo.style.opacity="1";
-    gwen.style.scale="1";
-    spiderMan.style.scale="1";
-    miles.style.scale="1";
-});
+    gwen.style.scale = 1;
+    gwen.style.top = 171+"px";
+    grupoGwen.style.display = "none";
+    miles.style.display = "block";
+    spiderMan.style.display = "block";
+    fondo.src = "img/fondos/rectanguloBlanco2.png"
+    lineaArriba.src = ""
+    lineaAbajo.src = ""
+})
 miles.addEventListener('mouseover', function(){
-    container.style.borderTop= "10px solid #304C71";
-    container.style.borderBottom= "10px solid #304C71";
-    layer.style.backgroundColor= "rgba(48, 76, 113, 0.2)";
-    fondo.style.opacity="0";
-    miles.style.scale="1.5";
-    gwen.style.scale="0.5";
-    spiderMan.style.scale="0.5";
-
-});
+    miles.style.scale = 1.5;
+    miles.style.top = 200+"px";
+    gwen.style.display="none"
+    spiderMan.style.display ="none";
+    grupoMorales.style.display = "block";
+    fondo.src = "img/fondos//grupo4/Rectangle 12.png"
+    lineaArriba.src = "img/fondos//grupo4/linea12Up.png"
+    lineaAbajo.src = "img/fondos//grupo4/linea12Down.png"
+})
 miles.addEventListener('mouseout', function(){
-    container.style.borderTop= "none";
-    container.style.borderBottom= "none";
-    layer.style.backgroundColor= "transparent";
-    fondo.style.opacity="1";
-    miles.style.scale="1";
-    gwen.style.scale="1";
-    spiderMan.style.scale="1";
-
-});
+    miles.style.scale = 1;
+    miles.style.top = 180+"px";
+    grupoMorales.style.display = "none";
+    gwen.style.display = "block";
+    spiderMan.style.display = "block";
+    fondo.src = "img/fondos/rectanguloBlanco2.png"
+    lineaArriba.src = ""
+    lineaAbajo.src = ""
+})
 spiderMan.addEventListener('mouseover', function(){
-    container.style.borderTop= "10px solid #2552C8";
-    container.style.borderBottom= "10px solid #2552C8";
-    layer.style.backgroundColor= "rgba(37, 82, 200, 0.2)";
-    spiderMan.style.scale="1.5";
-    gwen.style.scale="0.5";
-    miles.style.scale="0.5";
-});
+    spiderMan.style.scale = 1.3;
+    spiderMan.style.top = 200+"px";
+    gwen.style.display="none"
+    miles.style.display ="none";
+    grupoParker.style.display = "block";
+    fondo.src = "img/fondos//grupo4/Rectangle 11.png"
+    lineaArriba.src = "img/fondos//grupo4/linea11Up.png"
+    lineaAbajo.src = "img/fondos//grupo4/linea11Down.png"
+})
 spiderMan.addEventListener('mouseout', function(){
-    container.style.borderTop= "none";
-    container.style.borderBottom= "none";
-    layer.style.backgroundColor= "transparent";
-    spiderMan.style.scale="1";
-    gwen.style.scale="1";
-    miles.style.scale="1";
-});
+    spiderMan.style.scale = 1;
+    spiderMan.style.top = 180+"px";
+    grupoParker.style.display = "none";
+    gwen.style.display = "block";
+    miles.style.display = "block";
+    fondo.src = "img/fondos/rectanguloBlanco2.png"
+    lineaArriba.src = ""
+    lineaAbajo.src = ""
+})
 
